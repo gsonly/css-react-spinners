15c73fdfc20d017b6d0696c3bcded1c3d975bb69
tr1m <gsonly.imo@gmail.com>
2020-12-13 00:38:26 +0300

    add rollup and babel with react

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import { babel } from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
+const dist = 'dist'
+const bundle = 'bundle'
+
+const production = !process.env.ROLLUP_WATCH
+
+export default {
+  input: 'src/index.js',
+  external: ['react'],
+  output: [
+    {
+      file: `${dist}/${bundle}.cjs.js`,
+      format: 'cjs'
+    },
+    {
+      file: `${dist}/${bundle}.esm.js`,
+      format: 'esm'
+    },
+    {
+      name: 'CssReactSpinners',
+      file: `${dist}/${bundle}.umd.js`,
+      format: 'umd',
+      globals: {
+        react: 'React'
+      }
+    }
+  ],
+  plugins: [
+    resolve(),
+    babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' }),
+    production && terser()
+  ]
+}
